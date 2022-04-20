1bcd09e39d3cbf3e89d8618dd7bae4561c2fedc3
荻原 和樹 / Kazuki OGIWARA <kaz-ogiwara@users.noreply.github.com>
2022-04-20 17:53:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 import datetime
 import json
 import glob
+import os
 
 
 
@@ -72,11 +73,22 @@ def get_kaiji_url(kaiji):
 
 
 # Get CSV file content as array
+# Return False if the file does NOT exist
 def get_csv(url):
-  with open(url) as csvfile:
-    read = csv.reader(csvfile, delimiter = ',')
-    rows = list(read)
-    return rows
+  try:
+    with open(url) as csvfile:
+      read = csv.reader(csvfile, delimiter = ',')
+      rows = list(read)
+      return rows
+  except FileNotFoundError:
+    return False
+
+
+# Create folder if NOT exists
+def create_folder(url):
+  exists = os.path.exists(url)
+  if exists == False:
+    os.mkdir(url)
 
 
 # Save CSV or JSON file
@@ -91,7 +103,6 @@ def save_file(url, values):
   if ext == "json":
     with open(url, 'w') as f:
       json.dump(values, f, ensure_ascii = False)
-
 
 
 #-------------------------------------------------------------
@@ -292,44 +303,147 @@ def update_data(kaiji):
       return ret
 
 
+    def get_gian_file_header():
+      return [[
+        "審議回次",
+        "キャプション",
+        "種類",
+        "提出回次",
+        "番号",
+        "議案件名",
+        "審議状況",
+        "経過情報",
+        "経過情報URL",
+        "本文情報",
+        "本文情報URL",
+        "議案種類",
+        "議案提出回次",
+        "議案番号",
+        "議案件名",
+        "議案提出者",
+        "衆議院予備審査議案受理年月日",
+        "衆議院予備付託年月日／衆議院予備付託委員会",
+        "衆議院議案受理年月日",
+        "衆議院付託年月日／衆議院付託委員会",
+        "衆議院審査終了年月日／衆議院審査結果",
+        "衆議院審議終了年月日／衆議院審議結果",
+        "衆議院審議時会派態度",
+        "衆議院審議時賛成会派",
+        "衆議院審議時反対会派",
+        "参議院予備審査議案受理年月日",
+        "参議院予備付託年月日／参議院予備付託委員会",
+        "参議院議案受理年月日",
+        "参議院付託年月日／参議院付託委員会",
+        "参議院審査終了年月日／参議院審査結果",
+        "参議院審議終了年月日／参議院審議結果",
+        "公布年月日／法律番号"
+      ]]
+
+
+    # Convert kaiji row into individual gian row
+    def get_gian_file_row(krow):
+
+      row = [
+        kaiji,
+        krow[0],
+        krow[1],
+        krow[2],
+        krow[3],
+        krow[4],
+        krow[5],
+        krow[6],
+        krow[7],
+        krow[8],
+        krow[9],
+        krow[10],
+        krow[11],
+        krow[12],
+        krow[13],
+        krow[14],
+        krow[15],
+        krow[16],
+        krow[17],
+        krow[18],
+        krow[19],
+        krow[20],
+        krow[21],
+        krow[22],
+        krow[23],
+        krow[24],
+        krow[25],
+        krow[26],
+        krow[27],
+        krow[28],
+        krow[29],
+        krow[30]
+      ]
+
+      return row
+
+
     rows_kaiji = get_csv(DIR_DATA_KAIJI + "kaiji" + kaiji + ".csv")
-    rows_gian = get_csv(DIR_DATA + "gian_list.csv")
+    rows_gian_list = get_csv(DIR_DATA + "gian_list.csv")
 
-    for krow in rows_kaiji:
-
-      if krow[0] == "キャプション":
-        continue  # Skip header row
+    for krow in rows_kaiji[1:]:
 
       target_index = -1
 
-      for i, grow in enumerate(rows_gian):
-        if grow[0] == "議案種類":
-          continue  # Skip header row
+      for i, grow in enumerate(rows_gian_list):
+        if i == 0:
+          continue
         if get_gian_type(krow[0], krow[1]) == grow[0] and krow[2] == grow[1] and krow[3] == grow[2] and krow[4] == grow[3]:
           target_index = i
 
-      # Create a new row to add/update
-      target_row = [
+      # New row to add/update for gian_list
+      gian_list_row = [
         get_gian_type(krow[0], krow[1]),
         krow[2],
         krow[3],
         krow[4],
-        krow[2],
+        kaiji,
         get_gian_status(krow[5])
       ]
 
-      # When XXX already exists in gian_list
+      # New row to add/update for individual gian
+      gian_file_row = get_gian_file_row(krow)
+
+      # When the Gian already exists in gian_list
       if target_index >= 0:
-        rows_gian[target_index] = target_row
+        rows_gian_list[target_index] = gian_list_row
+        file_index = "g" + str(target_index).zfill(5) # ex. "g00001"
 
-      # When NOT exists
+      # When the Gian does NOT exist in gian_list
       else:
-        rows_gian.append(target_row)
+        rows_gian_list.append(gian_list_row)
+        file_index = "g" + str(len(rows_gian_list) - 1).zfill(5) # ex. "g00001"
 
-    save_file(DIR_DATA + "gian_list.csv", rows_gian)
-    save_file(DIR_DATA + "gian_list.json", rows_gian)
+      # Create DIR_DATA_GIAN + kaijiXXX folder if NOT exists
+      dir_data_gian_kaiji = DIR_DATA_GIAN + "kaiji" + krow[2] + "/"
+      create_folder(dir_data_gian_kaiji)
+
+      # Update individual gian file
+      rows_gian_file = get_csv(dir_data_gian_kaiji + file_index + ".csv")
+
+      if rows_gian_file == False:
+        rows_gian_file = get_gian_file_header()
+      else:
+        for j, frow in enumerate(rows_gian_file):
+          if j == 0:
+            continue
+          if kaiji == frow[0] and krow[0] == frow[1] and krow[1] == frow[2] and krow[2] == frow[3] and krow[3] == frow[4] and krow[4] == frow[5]:
+            rows_gian_file.pop(j)
+
+      # Save individual gian file
+      rows_gian_file.append(gian_file_row)
+      save_file(dir_data_gian_kaiji + file_index + ".csv", rows_gian_file)
+      save_file(dir_data_gian_kaiji + file_index + ".json", rows_gian_file)
+
+    # Save gian list file
+    save_file(DIR_DATA + "gian_list.csv", rows_gian_list)
+    save_file(DIR_DATA + "gian_list.json", rows_gian_list)
 
 
+  print("update_data(" + kaiji + ")")
   parse_kaiji(kaiji)
   update_gian_list(kaiji)
 
@@ -352,14 +466,18 @@ def update_time():
 
 DIR_DATA = get_content_folder()
 DIR_DATA_KAIJI = DIR_DATA + "kaiji/"
+DIR_DATA_GIAN = DIR_DATA + "gian/"
 GIAN_TYPE = get_static_file("gian_type")
 GIAN_STATUS = get_static_file("gian_status")
 LATEST = get_latest_kaiji()
 
-update_data(LATEST["latest"])
+#update_data(LATEST["latest"])
 
-if (LATEST["next"] != ""):
-  update_data(LATEST["next"])
+#if (LATEST["next"] != ""):
+  #update_data(LATEST["next"])
 
 update_time()
 
+
+for i in range(142,209):
+  update_data(str(i))
